83287067ee5b825bdc327916acf5c6b84e33e38c
Malice <mponoalice777@gmail.com>
2023-01-11 00:49:49 +0100

    async function rm from utils => script.js

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,8 +1,6 @@
-export const apiUrl = 'http://localhost:3000/api/products'
-
-export async function getData(){
+export const apiUrl = 'http://localhost:3000/api/products'//data's api
+ //simplified fetching function on api's url
+export async function getData(){                         
     const response = await fetch(apiUrl)
     const data = await response.json();
-    console.log(data)
-
 }
