456ae40e8f30eba20c36b57602a4c1ce864eca02
Malice <mponoalice777@gmail.com>
2023-01-11 01:54:45 +0100

    correcting the indentation of the code

--- front/js/script.js
+++ front/js/script.js
@@ -5,6 +5,7 @@ import {apiUrl} from "./utils.js";
     const response = await fetch(apiUrl)
     const data = await response.json();
 
+
     //looping through the fetched array to create an article for each elements 
     for (let i = 0; i< data.length; i++){
 
@@ -21,7 +22,7 @@ import {apiUrl} from "./utils.js";
     //article's content for each elements 
     //added in the DOM by modifying the innerHtml of the appended article
     article.innerHTML=(`
-    <img src= ${data[i].imageUrl} alt="${data[i].altTxt}">
+              <img src= ${data[i].imageUrl} alt="${data[i].altTxt}">
               <h3 class="productName">${data[i].name}</h3>
               <p class="productDescription">${data[i].description}</p>
     `
