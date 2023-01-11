bc92f73ce97426d2efbca895880f55440ec68d55
Malice <mponoalice777@gmail.com>
2023-01-11 01:59:18 +0100

    ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,33 @@
-import {apiUrl} from "./utils.js";
+import { apiUrl } from "./utils.js";
 
- //fetching data from the api's url with a simplified async function
- async function getData(){                         
+//fetching data from the api's url with a simplified async function
+async function getData() {
     const response = await fetch(apiUrl)
     const data = await response.json();
 
 
     //looping through the fetched array to create an article for each elements 
-    for (let i = 0; i< data.length; i++){
+    for (let i = 0; i < data.length; i++) {
 
-    //anchor and article for each elements,
-    //added in the DOM with .createElement and .appendchild
-    let a = document.createElement("a")
-    a.href ="./product.html?id="+ data[i]._id
-    let items =document.getElementById("items")
-    items.appendChild(a)
-    //
-    let article = document.createElement("article")
-    a.appendChild(article)
-     
-    //article's content for each elements 
-    //added in the DOM by modifying the innerHtml of the appended article
-    article.innerHTML=(`
+        //anchor and article for each elements,
+        //added in the DOM with .createElement and .appendchild
+        let a = document.createElement("a")
+        a.href = "./product.html?id=" + data[i]._id
+        let items = document.getElementById("items")
+        items.appendChild(a)
+        //
+        let article = document.createElement("article")
+        a.appendChild(article)
+
+        //article's content for each elements 
+        //added in the DOM by modifying the innerHtml of the appended article
+        article.innerHTML = (`
               <img src= ${data[i].imageUrl} alt="${data[i].altTxt}">
               <h3 class="productName">${data[i].name}</h3>
               <p class="productDescription">${data[i].description}</p>
     `
-    )
-}
+        )
+    }
 
 }
 //calling the function
