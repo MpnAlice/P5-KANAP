d9606dd6cc811247077f1edc5f50adc24d764a69
Malice <mponoalice777@gmail.com>
2023-01-11 01:57:01 +0100

    changing the name of the const and cleaning

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,6 +1,3 @@
-export const apiUrl = 'http://localhost:3000/api/products'//data's api
- //simplified fetching function on api's url
-export async function getData(){                         
-    const response = await fetch(apiUrl)
-    const data = await response.json();
-}
+//data's api
+export const apiUrl = 'http://localhost:3000/api/products'
+
