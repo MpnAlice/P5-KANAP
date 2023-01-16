ed00e060cefceeca8bc80a7d305f8e6b3abf4504
Malice <mponoalice777@gmail.com>
2023-01-16 13:56:21 +0100

    getting the pushed items and adding elements in the DOM

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,79 +1,70 @@
-import { apiUrl } from "./utils.js";
+//getting the pushed items from the localstorage
+let basket = localStorage.getItem("basket");
+console.log(basket)
 
-//
-const cart = document.querySelector('#cart__items')
+ //selecting the html element in which products will nest
+ const cart = document.querySelector('#cart__items')
+    
+ //adding elements in the DOM with "document.createElement()", ".appendChild", "innerHTML" and ".setAttributes" :
 
-//
-const article = document.createElement('article');
-article.setAttribute("class","cart__item");
-cart.appendChild(article)
+ //article
+ const article = document.createElement('article');
+ article.setAttribute("class", "cart__item");
+ cart.appendChild(article)
 
-//
-const imgContainer = document.createElement('div');
-imgContainer.setAttribute("class","cart__item__img" )
-article.appendChild(imgContainer)
-//
-const productImg = document.createElement('img');
-imgContainer.appendChild(productImg);
-productImg.src =''
-productImg.alt =''
+ // img
+ const imgContainer = document.createElement('div');
+ imgContainer.setAttribute("class", "cart__item__img")
+ article.appendChild(imgContainer)
+ //
+ const productImg = document.createElement('img');
+ imgContainer.appendChild(productImg);
+ 
+ //content
+ const productContent = document.createElement('div');
+ productContent.setAttribute("class", "cart__item__content");
+ article.appendChild(productContent)
+ //description
+ const description = document.createElement('div');
+ description.setAttribute("class", "cart__item__content__description");
+ productContent.appendChild(description);
+ description.innerHTML = `
+ <h2>Nom du produit</h2>
+ <p>Vert</p>
+ <p>42,00 €</p>
+ `
+ //settings
+ const settings = document.createElement('div');
+ settings.setAttribute("class", "cart__item__content__settings");
+ productContent.appendChild(settings);
 
-const productContent= document.createElement('div');
-productContent.setAttribute("class","cart__item__content");
-article.appendChild(productContent)
-//
-const description= document.createElement('div');
-description.setAttribute("class","cart__item__content__description");
-productContent.appendChild(description);
-description.innerHTML =`
-    <h2>Nom du produit</h2>
-    <p>Vert</p>
-    <p>42,00 €</p>
-`
-//setttings
-const settings= document.createElement('div');
-settings.setAttribute("class","cart__item__content__settings");
-productContent.appendChild(settings);
-//
-const quantitySettings = document.createElement('div');
-quantitySettings.setAttribute("class","cart__item__content__settings__quantity");
-settings.appendChild(quantitySettings)
+ //quantity
+ const quantitySettings = document.createElement('div');
+ quantitySettings.setAttribute("class", "cart__item__content__settings__quantity");
+ settings.appendChild(quantitySettings)
 
-//
-const p = document.createElement('p');
-p.innerText = "Qté : "
-quantitySettings.appendChild(p)
+ //
+ const p = document.createElement('p');
+ p.innerText = "Qté : "
+ quantitySettings.appendChild(p)
 
-//add item//reduce item//
-const quantityInput = document.createElement('input');
-quantityInput.setAttribute("class","itemQuantity");
-quantityInput.setAttribute("type","number");
-quantityInput.setAttribute("name","itemQuantity");
-quantityInput.setAttribute("min","1");
-quantityInput.setAttribute("max","100");
-quantityInput.setAttribute("value","");
+ //add item//reduce item//input
+ const quantityInput = document.createElement('input');
+ quantityInput.setAttribute("class", "itemQuantity");
+ quantityInput.setAttribute("type", "number");
+ quantityInput.setAttribute("name", "itemQuantity");
+ quantityInput.setAttribute("min", "1");
+ quantityInput.setAttribute("max", "100");
+ quantityInput.setAttribute("value", "");
 
-quantitySettings.appendChild(quantityInput);
+ quantitySettings.appendChild(quantityInput);
 
-//delete
-const deleteSettings = document.createElement('div');
-deleteSettings.setAttribute("class","cart__item__content__settings__delete");
-settings.appendChild(deleteSettings);
-
-//
-const deletebtn = document.createElement('p');
-deletebtn.setAttribute("class","deleteItem" );
-deletebtn.innerHTML =`Supprimer `;
-deleteSettings.appendChild(deletebtn);
-
-
-
-
-
-
-
-
-
-
-
-
+ //delete button
+ const deleteSettings = document.createElement('div');
+ deleteSettings.setAttribute("class", "cart__item__content__settings__delete");
+ settings.appendChild(deleteSettings);
+ //
+ const deletebtn = document.createElement('p');
+ deletebtn.setAttribute("class", "deleteItem");
+ deletebtn.innerHTML = `Supprimer `;
+ deleteSettings.appendChild(deletebtn);
