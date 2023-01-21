5578943b0e5032b18f91f4f3f810e071ea1fa9a7
Malice <mponoalice777@gmail.com>
2023-01-21 11:43:06 +0100

    adding the extraction of id and product url

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,4 +1,12 @@
 //api's url
 export const apiUrl = 'http://localhost:3000/api/products/'
+
+//extracting the id of the product from the end of the url's page
+const queryString_url = window.location.search
+// easy method to separate the id from the string
+const id = queryString_url.slice(4)
+//products's url
+export const productUrl = (`http://localhost:3000/api/products/${id}`);
 //
+
 
