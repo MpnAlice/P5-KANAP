b715c42d09b3a37bc86282cbe5edac98b51b5fad
Malice <mponoalice777@gmail.com>
2023-01-21 11:46:01 +0100

    adding export to id and querystring

--- front/js/utils.js
+++ front/js/utils.js
@@ -2,9 +2,9 @@
 export const apiUrl = 'http://localhost:3000/api/products/'
 
 //extracting the id of the product from the end of the url's page
-const queryString_url = window.location.search
+export const queryString_url = window.location.search
 // easy method to separate the id from the string
-const id = queryString_url.slice(4)
+export const id = queryString_url.slice(4)
 //products's url
 export const productUrl = (`http://localhost:3000/api/products/${id}`);
 //
