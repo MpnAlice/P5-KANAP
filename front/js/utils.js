2b90dd38325f2f8deccd0b90568491b3cfec4f4d
Malice <mponoalice777@gmail.com>
2023-02-02 13:41:14 +0100

    adding a function to create element with type and attributes parameters

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,3 +1,7 @@
+/**
+ * VARIABLES
+ */
+
 //api's url
 export const apiUrl = 'http://localhost:3000/api/products';
 //extracting the id of the product from the end of the url's page
@@ -6,3 +10,23 @@ export const queryString_url = window.location.search
 export const id = queryString_url.slice(4)
 //products's url
 export const productUrl = (`http://localhost:3000/api/products/${id}`);
+
+//
+
+/**
+ * FUNCTIONS
+ */
+
+//function to create elements 
+export function createElement(type, attributes) {
+    var element = document.createElement(type);
+    for (var key in attributes) {
+        if (key == "class") {
+            element.classList.add(element.classList, attributes[key]);
+        }
+        else {
+            element[key] = attributes[key]
+        }
+    }
+    return element
+}
