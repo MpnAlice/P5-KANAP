da87a07bf8a29b6fe5565efbf5ce2df493bd40ed
Malice <mponoalice777@gmail.com>
2023-02-06 12:23:15 +0100

    renaming the const

--- front/js/utils.js
+++ front/js/utils.js
@@ -5,28 +5,21 @@
 //api's url
 export const apiUrl = 'http://localhost:3000/api/products';
 //extracting the id of the product from the end of the url's page
-export const queryString_url = window.location.search
+export const queryString_url = new URLSearchParams(window.location.search)
 // easy method to separate the id from the string
-export const id = queryString_url.slice(4)
+export const paramId = queryString_url.get("id")
 //products's url
-export const productUrl = (`http://localhost:3000/api/products/${id}`);
+export const productUrl = (`http://localhost:3000/api/products/${paramId}`);
 
 //
+export const  items= document.querySelectorAll("items")
 
 /**
  * FUNCTIONS
  */
 
 //function to create elements 
-export function createElement(type, attributes) {
-    var element = document.createElement(type);
-    for (var key in attributes) {
-        if (key == "class") {
-            element.classList.add(element.classList, attributes[key]);
-        }
-        else {
-            element[key] = attributes[key]
-        }
-    }
+export function createElement(type) {
+    var element = document.createElement(type)
     return element
 }
