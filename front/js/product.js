0860dbf65439514d6fb62bb5b7213d4312a42e7d
Malice <mponoalice777@gmail.com>
2023-02-08 09:28:30 +0100

    correcting the add to cart function

--- front/js/product.js
+++ front/js/product.js
@@ -1,202 +1,177 @@
-import { productUrl, paramId, queryString_url, createElement } from "./utils.js";
+import { productUrl, id, queryString_url } from "./utils.js";
+//fetching the product's info from the new url 
 
-/**
- * retrieving the product's data from  the api with fetch
- */
-function fetchProductData() {
-    fetch(productUrl)
-        .then((res) => res.json())
-        .then((data) => {
-            implementingProductDetails(data);
-            addOnclick(data)
-        })
-        /**
-         * if the retrieval is not possible,an  alert will appear
-         */
-        .catch((err) => {
-            alert("une erreur est survenur lors de la récupération des données de votre produit depuis l'API")
+let product = ""
+const fetchProduct = async () => {
+    if (product) {
+       return product
+    }
+ 
+    //the products are elements of  api
+    try {
+       const response = await fetch(productUrl);
+       product = await response.json();
+       return product
+    }
+ 
+    //if there's an error in retrieving the datas,an alert will appear
+    catch (error) {
+       alert("une erreur est survenue lors du chargment de votre produit")
+ 
+    }
+ }
+ 
 
-        })
-}
-//calling the function
-fetchProductData();
+ fetchProduct()
 
-/**
-   implementing the product's elements in the DOM
- */
 
-function implementingProductDetails(data) {
+
+
+
+async function getProduct() {
+    const response = await fetch(productUrl)
+    const product = await response.json();
     //making image
     const ProductImage = document.createElement('img');
-    ProductImage.src = data.imageUrl;
-    ProductImage.alt = data.altTxt;
+    ProductImage.src = product.imageUrl;
+    ProductImage.alt = product.altTxt;
+    const url = product.imageUrl
     const imgParent = document.querySelector('.item__img');
     imgParent.appendChild(ProductImage);
-
     //adding Name
     const productName = document.querySelector('#title');
-    productName.innerText = data.name;
-    const name = data.name;
-
+    productName.innerText = product.name;
+    const name = product.name;
     //adding Price
     const productPrice = document.querySelector('#price');
-    productPrice.innerText = data.price;
-
+    productPrice.innerText = product.price;
 
     //adding Description
     const productDescription = document.querySelector('#description');
-    productDescription.innerText = data.description;
+    productDescription.innerText = product.description;
 
     //colors
     const colorSelection = document.querySelector('#colors');
-    let colorsPalette = data.colors;
-
+    let colorsPalette = product.colors;
     //looping through color array to:
     colorsPalette.forEach(color => {
         //create "option" tag in the DOM
-        const options = document.createElement("option");
-        colorSelection.appendChild(options);
+        const colorOption = document.createElement("option");
+        colorSelection.appendChild(colorOption);
         // setting element(s) of the array as value
-        options.value = color
+        colorOption.value = color
         // setting element(s) of the array as html element
-        options.innerHTML = color
-    })
-
-}
+        colorOption.innerHTML = color
+    });
 
 
-
-/**
- * ADDING PRODUCTS TO THE BASKET
- */
-
-
-//////////A FUNCTION TO ADD ELEMENTS ON CLICK
-function addOnclick(data) {
-
-
+    /**
+     * 
+     * adding to cart
+     */
 
     //initializing the button element present in the html
     const addToCartBtn = document.querySelector('#addToCart')
+    // setting the options of the selection elements in a function
+    const quantity = document.getElementById('quantity')
+
+    const colorChoice = document.getElementById('colors');
+
+
     addToCartBtn.addEventListener("click", (event) => {
 
-        // setting the options of the selection elements in a function
-        const quantity = document.getElementById('quantity')
-        const selectedQuantiy = parseInt(quantity.value)
-        const colorChoice = document.getElementById('colors');
-        const selectedColor = colorChoice.options[colorChoice.selectedIndex].value
-        //
         event.preventDefault();
+
+        const selectedColor = colorChoice.options[colorChoice.selectedIndex].value
+        const selectedQuantity = parseInt(quantity.value)
         //
 
-        /**
-         * creating an array with the color,quantity and id
-         */
-
-        let obj = {
-            id: data._id,
-            color: selectedColor,
-            quantity:selectedQuantiy,
+        if (selectedColor == "" || selectedQuantity > quantity.max || selectedQuantity < quantity.min || selectedQuantity <= 0) {
+            alert("veuillez  choisir une quantité comprise entre 1 et 100 ainsi qu'une couleur pour votre produit")
 
         }
 
-        //
+        if (selectedColor !== "" && selectedQuantity > 0 && selectedQuantity <= 100 && Number.isInteger(selectedQuantity)) {
 
-        if (selectedColor == "") {
-            alert("Merci d'indiquer la couleur choisie pour votre produit ")
-        }
+            let obj = {
+                id: product._id,
+                color: selectedColor,
+                quantity: selectedQuantity,
 
-        if (selectedQuantiy > quantity.max || selectedQuantiy < quantity.min) {
-            alert("veuillez  choisir une quantité comprise entre 1 et 100 pour votre produit")
-
-        }
-
-        else (
-            // calling the function which add the obj in the basket
-            addToBasket(obj)
-
-        )
-
-
-
-    });
-
-}
-
-
-function addToBasket(obj) {
-    let basket = JSON.parse(localStorage.getItem("localproduct"));
-    //finding an object with same color and id
-    let foundProduct = basket.find((item) => item.id === obj.id && item.color == obj.color)
-
-    if (foundProduct) {
-        // the obj .quantity is the same as the fround product
-        foundProduct.quantity += obj.quantity;
-
-        basket = basket.map((item) => {
-
-
-            //if the obj id is the same as the  item in the new array 
-            if (obj.id == item.id && obj.color == item.color) {
-                return foundProduct
             }
-            //return the item which is the ancien obj with a new quantity 
-
-            return item;
-
-
-        });
+            console.log(obj)
 
 
 
+            let basket = JSON.parse(localStorage.getItem("localproduct")) || [];
+            const foundProduct = basket.find(
+                (elt) =>
+                    elt.id == obj.id && elt.color == obj.color);
 
-    } else {
 
-        //add the new obj into the basket
-        basket = []
-        basket.push(obj);
+            if (basket) {
+
+                if (foundProduct) {
+                    // the obj .quantity is the same as the fround product
+                    let addQuantity = parseInt(obj.quantity) + parseInt(foundProduct.quantity)
+                    foundProduct.quantity = addQuantity
+                }
+
+                
+                basket.push(obj)
+                localStorage.setItem("localproduct", JSON.stringify(basket))
+                alert("votre nouvel article a bien été ajouté au panier")
+
+
+                if (confirm("souhaitez- vous accéder au panier?") == true) {
+                    window.location.href = "./cart.html";
+                }
+                //else {
+                   // (confirm("d'accord, souhaitez-vous continuer vos achats ?") == true)
+                   // window.location.href = "./index.html";
     
-    }
+                //}
 
-   
-   
- 
 
-}
 
-/**
- * GO TO CART CONST
- */
-const goToCart =() =>{
-    if(confirm ("votre commande est bien enregistrée,souhaitez vous accéder à votre panier?")== true)
-    window.location.href = "./cart.html"
+            }
+        }
+    })
+    //
+};
 
-}
+getProduct()
 
 /**
- * ADD TO BASKET
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 
-const setToBasket =()=> localStorage.setItem('localproduct', JSON.stringify(basket))
 
 
-
-
-
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
